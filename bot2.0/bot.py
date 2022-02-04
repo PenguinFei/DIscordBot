5ef6cf171adfbd01f95888c0c827634475570ba0
PenguinFei <78941973+PenguinFei@users.noreply.github.com>
2022-02-04 22:11:35 +0800

    Add files via upload

---
+++ bot2.0/bot.py
@@ -0,0 +1,38 @@
+from discord.ext import commands 
+import discord
+import json
+import random
+import os
+
+with open('settings.json', 'r', encoding = 'utf8') as jfile:
+    jdata = json.load(jfile)
+
+bot = commands.Bot(command_prefix="$")
+    
+@bot.event
+async def on_ready():
+    print("<<<Bot in ready>>>")
+
+@bot.command()
+async def load(ctx, extention):
+    bot.load_extension(f'command.{extention}')
+    await ctx.send(f"Loaded {extention} done")
+
+@bot.command()
+async def unload(ctx, extention):
+    bot.unload_extension(f'command.{extention}')
+    await ctx.send(f"Un-Loaded {extention} done")
+
+@bot.command()
+async def reload(ctx, extention):
+    bot.reload_extension(f'command.{extention}')
+    await ctx.send(f"Re-Loaded {extention} done")
+
+
+##顯示分類名稱
+for Filename in os.listdir('./command'):
+    if Filename.endswith('.py'):
+        bot.load_extension(F'command.{Filename[:-3]}')
+
+if __name__ == "__main__":
+    bot.run(jdata['token'])  
---
+++ bot2.0/command/pixiv.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extention
+import requests
+
+class pixiv(Cog_Extention):
+    @commands.command()
+    async def pix(self, ctx,* ,url):
+        r = requests.get(url)
+        if(r.status_code == requests.codes.ok):
+            await ctx.send('https://pixiv.cat/'+ url[-8:] + '.jpg')
+       
+def setup(bot):
+    bot.add_cog(pixiv(bot))
