7ef4e4774eac0222a50db13720f2f4d16185d647
PenguinFei <78941973+PenguinFei@users.noreply.github.com>
2022-02-19 18:15:44 +0800

    Add files via upload

---
+++ bot4.0/command/main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extention
+
+class Main(Cog_Extention):
+    ##ping值
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'{round(self.bot.latency*1000)}(ms)')
+
+    @commands.command()
+    async def info(self, ctx):
+        embed=discord.Embed(title="LaPenGuin", url="https://github.com/PenguinFei/DIscordBot", description="Click here to Github for more info", color=0xb020f3)
+        embed.set_author(name="PenguinFei", icon_url="https://i.imgur.com/KAHGCeT.jpg")
+        embed.set_thumbnail(url="https://i.imgur.com/tBn6LRP.jpeg")
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Main(bot))
